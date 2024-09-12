51365660661c15e1c33c6142c1ea0e06044900cb
DanielGarciaPluma <230187@utxicotepec.edu.mx>
2024-09-12 17:10:21 -0600

    Practica 03
    Agregación del ejercici 02: Tipos de datos Unidefined, boolean, number.

---
+++ JS/Ejercicio02.js
@@ -0,0 +1,57 @@
+//tipos de datos en JS
+
+//1.-Unidefined
+console.warn("---Tipo de dato: Unidefined");
+
+let cliente;
+console.log(`EL ciente es: ${cliente}`);
+console.log(`El tipo de dato de la variable es: ${typeof(cliente)}`);
+
+cliente="Juan Romero";
+console.log(`EL ciente es: ${cliente}`);
+console.log(`El tipo de dato de la variable es: ${typeof(cliente)}`);
+
+cliente="83334";
+console.log(`EL ciente es: ${cliente}`);
+console.log(`El tipo de dato de la variable es: ${typeof(cliente)}`);
+
+
+//2.-Boolean
+console.warn("---Tipo de dato: Boolean (boleano - true/false)");
+let esPremiun ="No lo se";
+console.log(`¿Es el cliente Premiun?: ${esPremiun}`);
+console.log(`El tipo de dato de la variable esPremiun es: ${typeof(esPremiun)}`);
+console.log('Asignacion en el valor "true" a la variable')
+esPremiun="true";
+console.log(`El tipo de dato de la variable esPremiun es: ${typeof(esPremiun)}`);
+console.log("Asignacion en el valor true a la variable")
+
+esPremiun=true;
+console.log(`El tipo de dato de la variable esPremiun es: ${typeof(esPremiun)}`);
+
+console.log(`¿Es el cliente Premiun?: ${esPremiun}`);
+if(esPremiun){
+console.log("El cliente pago por usar el servicio.");
+
+}else{
+console.log("El cliente resive los servicios gratuitos.");
+}
+
+
+//3.-Number
+var cantidad;
+const costo_produccion = 10.50;
+let saldo_cuenta =2500.40;
+let monto_transaccion;
+
+console.warn("--Tipo de Dato - Number (Numeros, posistivos, negativos, decimales, flotantes)");
+console.log(`tu saldo al dia de hoy es de: ${saldo_cuenta}, (Tipo de dato = ${typeof(saldo_cuenta)} ) `);
+console.log(`EL producto que hasselccionado cuesta: ${costo_produccion}`);
+cantidad=8;
+console.log(`Has elegido comprar: ${cantidad} de productos`);
+console.log(`El importe total de la compra es: ${cantidad}*${costo_produccion} `);
+
+console.log(`Tu nuevo total de la compra es de: ${saldo_cuenta}-${cantidad}*${costo_produccion}`)
+
+
+
