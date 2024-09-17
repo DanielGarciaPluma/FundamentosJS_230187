4f786f31cd03f1d6fdab08ab8293fa308fd8cfb0
DanielGarciaPluma <230187@utxicotepec.edu.mx>
2024-09-17 15:25:12 -0600

    Practica 04
    Agregacion de tipos de datos nuevis.

---
+++ JS/Ejercicio03.js
@@ -0,0 +1,36 @@
+// // String o cadenas de texto
+// const alumno = "Juan";
+// let producto = "Monitor 20 pulgadas";
+
+// const numero = "30";
+// nost numero2 =30;
+// console.log (type numero);
+// console.log(typeof numero2);
+
+//Bignit
+// const numeroGrande = BigInt (769696969646875987);
+// console.log(typeof numeroGrande);
+
+// const numero = 10;
+// const numero2 =20;
+// console.log( numero= Number(numeroGrande));
+
+
+// const numero = "30";
+// const numer2 = 30;
+// console.log(typeof String (numero));
+// console.log(typeof Number(numer2));
+
+// //Symbol
+// const primerSymbol = Symbol(30);
+// const segundoSymbol = Symbol(30);
+// console.log(primerSymbol === segundoSymbol);
+// console.log(primerSymbol.valueOf());
+// console.log(segundoSymbol.valueOf());
+
+
+
+// //null
+// const descuento =null;
+// console.log(typeof descuento);
+
